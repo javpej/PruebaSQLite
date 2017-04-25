ffeebda7c09d46a728152950694b5a310dd86f11
Javi Perán <javperan@gmail.com>
2017-04-25 11:57:26 +0200

    Termina Toolbar y Menus. Todo Ok

--- app/src/main/java/com/example/javi/pruebasqlite/MainActivity.java
+++ app/src/main/java/com/example/javi/pruebasqlite/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.javi.pruebasqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.about_action:
+
+                Toast.makeText(this, "Has pulsado Acerca de...", Toast.LENGTH_SHORT).show();
+
+            case R.id.add_action:
+
+                Toast.makeText(this, "Has pulsado Añadir", Toast.LENGTH_SHORT).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 }
